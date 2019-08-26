c978dda0cc0d8624eae96e9ba3d609fac86c2e20
Edmund Dantes <d.ajaykumar.017@gmail.com>
2019-08-26 23:37:47 +0530

    Serve static files on /static route.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const serveStatic = require('serve-static');
 const apiRoutes = require('./routes/api');
+
+app.locals.rootDirectory = process.cwd();
 
 app.get('/', (req, res) => res.send(new Date().toString()))
 app.use('/api', apiRoutes);
-
-app.locals.rootDirectory = process.cwd();
+app.use('/static', serveStatic(app.locals.rootDirectory))
 
 module.exports = app;
