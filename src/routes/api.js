fc05c6ab3749ada798968f9d168a47f93309995f
Edmund Dantes <d.ajaykumar.017@gmail.com>
2019-09-06 11:54:42 +0530

    Send 400 if asked folder does not exist.

--- src/routes/api.js
+++ src/routes/api.js
@@ -7,8 +7,9 @@ const util = require('util');
 const fsExists = util.promisify(fs.exists);
 const { getFiles } = require('../../lib');
 
-router.get('/ls', async function(req, res) {
-    const { rootDirectory } = req.app.locals; 
+router.get('/ls', async function (req, res) {
+    util.log(`/ls`);
+    const { rootDirectory } = req.app.locals;
     var { cwd } = req.query;
 
     if (!cwd)
@@ -16,10 +17,13 @@ router.get('/ls', async function(req, res) {
 
     const requestedDirectory = path.join(rootDirectory, cwd);
 
-    if (! await fsExists(requestedDirectory))
-        throw new Error(`Folder ${cwd} does not exist!`);
+    if (! await fsExists(requestedDirectory)) {
+        util.log(`${cwd}: User tried to access a folder that does not exist!`);
+        return res.status(400).json({ error: `Folder ${cwd} does not exist!` });
+    }
 
-    res.json(await getFiles(requestedDirectory));
+    util.log(`${cwd}: GET list.`);
+    return res.json(await getFiles(requestedDirectory));
 });
 
 module.exports = router;
